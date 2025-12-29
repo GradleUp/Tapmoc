ba16a24ec66e8d0ba4893458695b5b4e97eecf40
Martin Bonnin <martin@mbonnin.net>
2025-12-29 19:22:10 +0100

    Bump version to 0.4.0-SNAPSHOT (#77)

--- scripts/update-repo.main.kts
+++ scripts/update-repo.main.kts
@@ -1,9 +1,11 @@
 #!/usr/bin/env kotlin
 
 @file:Repository("https://repo.maven.apache.org/maven2/")
+@file:Repository("https://dl.google.com/android/maven2/")
 @file:Repository("https://storage.googleapis.com/gradleup/m2")
 @file:Repository("https://jitpack.io")
-@file:DependsOn("com.gradleup.librarian:librarian-cli:0.1.1-SNAPSHOT-41af255deeb3b644524aed9ffe4c64aad5ebbd0b")
+//@file:Repository("file://~/.m2/repository")
+@file:DependsOn("com.gradleup.librarian:librarian-cli:0.2.2-SNAPSHOT-0cbca80f60a15a0f851a9cd468bfc352db316dd4")
 
 import com.gradleup.librarian.cli.updateRepo
 
